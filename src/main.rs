81db40b124ddf591dc80127a3732f07ba4268bfb
Edgar Montiel Cruz <yzen1990@outlook.com>
2024-11-15 00:55:50 -0500

    Preliminary implementation of initial dns records sync and interval batch updates

--- src/main.rs
+++ src/main.rs
@@ -5,69 +5,104 @@ mod constants;
 mod types;
 mod utils;
 
-use anyhow::{Context, Error, Ok, Result};
+use anyhow::{anyhow, Context, Error, Result};
 use configuration::Configuration;
 use log::{debug, info, warn};
 use std::{thread::sleep, time::Duration};
 use ureq::{Agent, AgentBuilder, Error as RequestError};
 
 use constants::AUTHORIZATION_HEADER;
-use types::{Batch, Record, RecordCreate, RecordUpdate, Records, IP};
+use types::*;
 
 fn main() -> Result<()> {
   let configuration = configuration::load()?;
-
   utils::logging::initialize(&configuration)?;
 
   if configuration.records.is_empty() {
     warn!("No records provided, nothing to do!");
   } else {
     let interval = configuration.interval.unwrap_or(Duration::ZERO);
-
     let client = AgentBuilder::new().https_only(true).build();
-    let base_url = format!("https://api.cloudflare.com/client/v4/zones/{}/dns_records", configuration.zone);
 
-    let mut record_ids = Vec::<String>::with_capacity(configuration.records.len());
-    let mut last_ip: String;
+    let (records_ids, mut last_ip, url) = sync_records(&client, &configuration)?;
 
-    {
-      let dns_records_url = format!("{base_url}?type=A");
-      let records = dns_records(&client, &dns_records_url, &configuration).context("DNS records retrieval")?;
-      last_ip = public_ip(&client).context("Public IP retrieval")?;
-
-      let mut existing = Vec::<String>::new();
-      let mut record_updates = Vec::<RecordUpdate>::new();
-      let mut new_records = Vec::<RecordCreate>::new();
-
-      for record in records {
-        if let Some(_) = configuration.records.iter().find(|&name| *name == record.name) {
-          existing.push(record.name);
-          record_ids.push(record.id.clone());
-
-          if record.content != last_ip {
-            update_batch.push(RecordUpdate { id: record.id, content: last_ip.clone() });
-          }
-        }
-      }
-
-      for record in configuration.records {
-        if let Some(_) = existing.iter().find(|&name| *name == record) {}
-      }
-    }
+    info!("Records IDs: {:#?}", records_ids);
 
     if !interval.is_zero() {
       loop {
         sleep(interval);
-        let current_ip = public_ip(&client).context("Public IP retrieval")?;
 
+        let current_ip = public_ip(&client).context("Public IP retrieval")?;
         if current_ip != last_ip {
           last_ip = current_ip;
+
+          dns_batch(
+            &client,
+            &url,
+            &configuration,
+            Batch {
+              patches: records_ids.iter().map(|id| RecordUpdate { id: id.clone(), content: last_ip.clone() }).collect(),
+            },
+          )?;
+
+          info!("Public IP changed, the DNS records have been updated")
         }
       }
     }
   }
 
   Ok(())
+}
+
+fn sync_records(client: &Agent, configuration: &Configuration) -> Result<(Vec<String>, String, String)> {
+  let mut base_url = format!("https://api.cloudflare.com/client/v4/zones/{}/dns_records", configuration.zone);
+  let dns_records_url = format!("{base_url}");
+
+  let records_list = dns_records(&client, &dns_records_url, &configuration).context("DNS records retrieval")?;
+  let last_ip = public_ip(&client).context("Public IP retrieval")?;
+
+  let mut records = configuration.records.clone();
+  let mut records_ids = Vec::<String>::with_capacity(records.len());
+  let mut changed_records = Vec::<RecordUpdate>::new();
+  let mut new_records = Vec::<RecordCreate>::new();
+
+  info!("zone records: {}", records_list.len());
+  info!("records: {}", records.len());
+
+  for record in records_list {
+    if let Some(index) = records.iter().position(|name| *name == record.name) {
+      records_ids.push(record.id.clone());
+
+      if record.content != last_ip {
+        changed_records.push(RecordUpdate { id: record.id, content: last_ip.clone() });
+      }
+
+      records.swap_remove(index);
+      if records.len() == 0 {
+        break;
+      }
+    }
+  }
+
+  info!("new records: {}", records.len());
+
+  for record in records {
+    new_records.push(RecordCreate { name: record.clone(), content: last_ip.clone(), ttl: TTL::TTL });
+  }
+
+  base_url.push_str("/batch");
+  if !new_records.is_empty() {
+    records_ids.extend(dns_batch_with_create(
+      &client,
+      &base_url,
+      &configuration,
+      BatchWithCreate { patches: changed_records, posts: new_records },
+    )?);
+  } else if !changed_records.is_empty() {
+    dns_batch(&client, &base_url, &configuration, Batch { patches: changed_records })?;
+  }
+
+  Ok((records_ids, last_ip, base_url))
 }
 
 fn public_ip(client: &Agent) -> Result<String> {
@@ -84,25 +119,58 @@ fn public_ip(client: &Agent) -> Result<String> {
 
 fn dns_records(client: &Agent, url: &str, configuration: &Configuration) -> Result<Vec<Record>> {
   debug!("Retrieving DNS records for zone: {}", configuration.zone);
-  Ok(
-    client
-      .get(url)
-      .set(AUTHORIZATION_HEADER, &configuration.api_token)
-      .call()
-      .map_err(|error| match error {
-        RequestError::Status(code, _) => match code {
-          404 => Error::new(error).context("Invalid zone"),
-          400 => Error::new(error).context("Invalid API token"),
-          _ => Error::new(error),
-        },
+  let data: Records = client
+    .get(url)
+    .set(AUTHORIZATION_HEADER, &configuration.api_token)
+    .call()
+    .map_err(|error| match error {
+      RequestError::Status(code, _) => match code {
+        404 => Error::new(error).context("Invalid zone"),
+        400 => Error::new(error).context("Invalid API token"),
         _ => Error::new(error),
-      })?
-      .into_json::<Records>()
-      .context("Unexpected Cloudflare API response")?
-      .result,
-  )
+      },
+      _ => Error::new(error),
+    })?
+    .into_json()
+    .context("Unexpected Cloudflare API response")?;
+
+  if data.errors.is_empty() {
+    Ok(data.result)
+  } else {
+    Err(anyhow!(flatten_error(data.errors)))
+  }
 }
 
-fn dns_batch(records: Batch) -> Result<()> {
-  Ok(())
+fn dns_batch(client: &Agent, url: &str, configuration: &Configuration, batch: Batch) -> Result<()> {
+  debug!("Sending DNS records batch");
+  let data: BatchResult =
+    client.get(url).set(AUTHORIZATION_HEADER, &configuration.api_token).send_json(batch)?.into_json()?;
+
+  if data.errors.is_empty() {
+    Ok(())
+  } else {
+    Err(anyhow!(flatten_error(data.errors)))
+  }
+}
+
+fn dns_batch_with_create(
+  client: &Agent,
+  url: &str,
+  configuration: &Configuration,
+  batch: BatchWithCreate,
+) -> Result<Vec<String>> {
+  debug!("Sending DNS records batch with new records");
+
+  let data: BatchWithCreateResult =
+    client.get(url).set(AUTHORIZATION_HEADER, &configuration.api_token).send_json(batch)?.into_json()?;
+
+  if data.errors.is_empty() {
+    Ok(data.result.posts.iter().map(|record| record.id.clone()).collect())
+  } else {
+    Err(anyhow!(flatten_error(data.errors)))
+  }
+}
+
+fn flatten_error(errors: Vec<APIError>) -> String {
+  errors.iter().map(|APIError { code, message }| format!("[{code}] {message} ")).collect()
 }
--- src/types.rs
+++ src/types.rs
@@ -8,13 +8,41 @@ pub struct Record {
 }
 
 #[derive(Deserialize)]
+pub struct APIError {
+  pub code: u32,
+  pub message: String,
+}
+
+#[derive(Deserialize)]
 pub struct Records {
   pub result: Vec<Record>,
+  pub errors: Vec<APIError>,
 }
 
 #[derive(Deserialize)]
 pub struct IP {
   pub ip: String,
+}
+
+#[derive(Deserialize)]
+pub struct BatchResult {
+  pub errors: Vec<APIError>,
+}
+
+#[derive(Deserialize)]
+pub struct NewRecord {
+  pub id: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateResult {
+  pub posts: Vec<NewRecord>,
+}
+
+#[derive(Deserialize)]
+pub struct BatchWithCreateResult {
+  pub result: CreateResult,
+  pub errors: Vec<APIError>,
 }
 
 #[derive(Serialize)]
@@ -25,14 +53,26 @@ pub struct RecordUpdate {
 }
 
 #[derive(Serialize)]
+pub enum TTL {
+  #[serde(rename = "60")]
+  TTL,
+}
+
+#[derive(Serialize)]
 #[serde(tag = "type", rename = "A")]
 pub struct RecordCreate {
   pub name: String,
   pub content: String,
+  pub ttl: TTL,
 }
 
 #[derive(Serialize)]
 pub struct Batch {
-  pub patches: Option<Vec<RecordUpdate>>,
-  pub posts: Option<Vec<RecordCreate>>,
+  pub patches: Vec<RecordUpdate>,
+}
+
+#[derive(Serialize)]
+pub struct BatchWithCreate {
+  pub patches: Vec<RecordUpdate>,
+  pub posts: Vec<RecordCreate>,
 }
